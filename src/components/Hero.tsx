a0f1ba96fe8c7373240d04d797c644ea337938da
LRMNTRIX <rizaldilutfi1@gmail.com>
2026-02-24 09:26:29 +0700

    ganti cv

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useIntersectionObserver } from '../hooks/useIntersectionObserver';
 import { Github, Linkedin, Mail, Download, ArrowRight } from 'lucide-react';
 import pp_keren from '../assets/img/pp_keren.jpeg';
-import cv from '../assets/cv/CV_Lutfi_Rizaldi_Mahida - 2.pdf';
+import cv from '../assets/cv/CV_Lutfi_Rizaldi_Mahida - new.pdf';
 
 const Hero: React.FC = () => {
   const { elementRef, isIntersecting } = useIntersectionObserver({ 
